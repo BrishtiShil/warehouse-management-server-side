d4227c7a107ef42dc2cb7b66716e72ea974b158c
Brishti Shil <brishtishil62@gmail.com>
2022-05-08 13:15:28 +0600

    adding steps

--- index.js
+++ index.js
@@ -50,21 +50,6 @@ async function run() {
             res.send(result);
         })
 
-        // update inventory
-        app.put('inventory/:id', async (req, res) => {
-            const id = req.params.id;
-            const addQuantity = req.body;
-            const filter = { _id: ObjectId(id) };
-            const options = { upsert: true };
-            const updateInventory = {
-
-                $set: {
-                    quantity: newQuantity[0].quantity,
-                }
-            };
-            const result = await inventoryCollection.updateOne(filter, updateInventory, options);
-            res.send(result);
-        })
 
         // DELETE
         app.delete('/inventory/:id', async (req, res) => {
